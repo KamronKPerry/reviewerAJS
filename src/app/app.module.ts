a8a0938e1c6ca107dccf00d3ab72d227ff974b1a
kyzight <kyzight@gmail.com>
2017-11-05 19:26:49 -0600

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AdminComponent } from './admin/admin.component';
+import { SurveyComponent } from './survey/survey.component';
+import { LoginComponent } from './login/login.component';
+import { SurveyRequestComponent } from './survey-request/survey-request.component';
+
+// CanActivate Guards with all but Survey
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    SurveyComponent,
+    LoginComponent,
+    SurveyRequestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'Admin',
+        component: AdminComponent
+      },
+      {
+        path: 'Survey',
+        component: SurveyComponent
+      },
+      {
+        path: 'Login',
+        component: LoginComponent
+      },
+      {
+        path: 'SurveyRequest',
+        component: SurveyRequestComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {   }
